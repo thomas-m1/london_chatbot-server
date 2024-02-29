7ffd3efb1ca5d226862b7125b07775dd48030bb1
Dhruwang Akbari <dhruwang.akbari@gmail.com>
2024-02-29 16:49:58 -0500

    update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@
 
 import os
 import time
-import openai, langchain, pinecone
-# from openai import OpenAI
+import traceback, logging
+# import openai, langchain, pinecone
+
 
 
 from flask import Flask, request, jsonify
@@ -11,80 +12,85 @@ from langchain_community.document_loaders import DirectoryLoader, TextLoader, Un
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 
-from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 from langchain_community.llms import OpenAI
 
+# from openai import OpenAI
 from langchain.chains import RetrievalQA
-from pinecone import Pinecone, ServerlessSpec
 from langchain.vectorstores import Pinecone as PineconeStore
+from langchain_pinecone import PineconeVectorStore
 from langchain.chains.question_answering import load_qa_chain
 from langchain.chains import RetrievalQA
-
-os.environ["OPENAI_API_KEY"] = "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"
-OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-
-embed_model = "text-embedding-ada-002"
-
-os.environ["PINECONE_API_KEY"] = "c31d352a-23fe-4db1-b66b-4acc33308fcf"
-PINECONE_API_KEY = os.environ['PINECONE_API_KEY']
-PINECONE_ENV = "us-west4-gcp-free"
+from pinecone import Pinecone as PineconeClient, ServerlessSpec
+from config.pinecone import OPENAI_API_KEY, PINECONE_API_KEY, PINECONE_ENV, PINECONE_INDEX_NAME, PINECONE_NAME_SPACE
 
 
 app = Flask(__name__)
 
 # Open the data file and read its content
+def chatbot_vector_store(filePath):
+    
+    try:
+        loader = DirectoryLoader(filePath, glob="./*.pdf", loader_cls=PyPDFLoader)
+        documents = loader.load()
 
-loader = DirectoryLoader('./docs', glob="./*.pdf", loader_cls=PyPDFLoader)
-documents = loader.load()
+        # Set up the RecursiveCharacterTextSplitter, then Split the documents
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        texts = text_splitter.split_documents(documents)
+    
+        print("split docs:", texts)
+        print("\n")
+        print("creating a vector store...")
+        pc = PineconeClient(api_key=PINECONE_API_KEY)
+    
+        # index_name = "testingindex"
+        index = pc.Index(PINECONE_INDEX_NAME)
 
-
-# Set up the RecursiveCharacterTextSplitter, then Split the documents
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-texts = text_splitter.split_documents(documents)
-
-
-
-pc = Pinecone(api_key=PINECONE_API_KEY)
-index_name = "testingindex"
-index = pc.Index(index_name)
-
-if index_name not in pc.list_indexes().names():
-    print("Index does not exist: ", index_name)
-    pc.create_index(
-        name=index_name,
-        dimension=1536,
-        metric="cosine",
-        spec=ServerlessSpec(
-            cloud='aws',
-            region='us-west-2'
-        )
-    )
-    # wait for index to be initialized
-    while not pc.describe_index(index_name).status['ready']:
-        time.sleep(1)
-    # # docsearch = Pinecone.from_documents(texts, embeddings, index_name = index_name)
-else:
-    print("Index exists: ", index_name)
-    # docsearch = Pinecone.from_existing_index(index_name, embeddings)
-index_stats = index.describe_index_stats()
-# print (index_stats)
-
-vector_count = index_stats['total_vector_count']
-vector_count_str = str(vector_count)
-
-# index_description = pc.describe_index(index_name)
-
-
-if vector_count >=0:
-    # Prepare the embedding so that we can pass it to the pinecone call in the next step
-    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-    # Create the vector store (new vector store)
-    docsearch = PineconeStore.from_documents(texts, embeddings, index_name = index_name)
-    # for existing an vector store
-    # docsearch = PineconeStore.from_existing_index(index_name, embeddings)
-
-
+        if PINECONE_INDEX_NAME not in pc.list_indexes().names():
+            print("Index does not exist: ", PINECONE_INDEX_NAME)
+            pc.create_index(
+                name=PINECONE_INDEX_NAME,
+                dimension=1536,
+                metric="cosine",
+                spec=ServerlessSpec(
+                    cloud='aws',
+                    region='us-west-2'
+                )
+            )
+            # wait for index to be initialized
+            while not pc.describe_index(PINECONE_INDEX_NAME).status['ready']:
+                time.sleep(1)
+            # # docsearch = Pinecone.from_documents(texts, embeddings, index_name = index_name)
+        else:
+            print("Index exists: ", PINECONE_INDEX_NAME)
+            # docsearch = Pinecone.from_existing_index(index_name, embeddings)
+        index_stats = index.describe_index_stats()
+        print(index_stats)
+        vector_count = index_stats['total_vector_count']
+        print(vector_count)
+        if vector_count >=0:
+            # Prepare the embedding so that we can pass it to the pinecone call in the next step
+            print("Hello world!")
+            embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+            # Create the vector store (new vector store)
+            # pinecone_index = PineconeVectorStore.get_pinecone_index
+            # print(pinecone_index)
+            vector_db = PineconeVectorStore.from_texts(
+                texts, 
+                embedding=embeddings, 
+                index_name=PINECONE_INDEX_NAME)
+            print("Hello!!")
+            global doc_retriever 
+            doc_retriever = vector_db.as_retriever()
+            
+            # for existing an vector store
+            # docsearch = PineconeStore.from_existing_index(index_name, embeddings)
+        
+        print("Ingestion Complete.")
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        print("Ingestion Incomplete.")
 
 # # Helper function to process the response from the QA chain
 # # and isolate result and source docs and page numbers
@@ -93,112 +99,61 @@ def parse_response(response):
     print('\n\nSources:')
     for source_name in response["source_documents"]:
         print(source_name.metadata['source'], "page #:", source_name.metadata['page'])
-# from openai import OpenAI
 
 
-def chatbot_response_gpt(findTemp):
-    from openai import OpenAI
-
-    client = OpenAI(
-        #   organization='YOUR_ORG_ID',
-        api_key=os.environ.get(OPENAI_API_KEY),
-
-    )
-
+def chatbot_get_temp(findTemp):
+    client = OpenAI(api_key=OPENAI_API_KEY)
     qa_prompt = f"I want you to give me a rating on a scale of 0.0-1.0... 0.0 being a more severe situation that needs a prompt response, and a 1.0 being a less serious situation and can answer the phrase in a lighthearted manner. I only want the rating and nothing else. here is the phrase: {findTemp}"
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": "you are a virtual assistant"},
         {"role": "user", "content": qa_prompt},
-         #f"I want you to give me a rating on a scale of 0.0-1.0... 0.0 being a more severe situation that needs a prompt response, and a 1.0 being a less serious situation and can answer the phrase in a lighthearted manner. I only want the rating and nothing else. here is the phrase: {findTemp}"}
     ]
     )
-    print(completion.choices[0].message.content)
-
+    
+    # print(completion)
     assistant_reply = completion.choices[0].message
-
-    # Extract the assistant's reply from the response
+    print("temp : ", assistant_reply)
     return assistant_reply
 
 # Set up the retriever on the pinecone vectorstore
-retriever = docsearch.as_retriever(include_metadata=True, metadata_key = 'source')
+# retriever = docsearch.as_retriever(include_metadata=True, metadata_key = 'source')
 
 @app.route("/chatbot", methods=['POST'])
 
 def chatbot():
     
+    doc_retriever 
     data = request.get_json()
-    userFindTemp = data['message']
+    query = data['message']
+    print("user message: ", query)
     # userFindTemp = "hey I just fell and got a small cut on my leg after I tripped. it is not bad. what should I do?"
-    response_message = chatbot_response_gpt(userFindTemp)
-    adjustedTemp = response_message.content
+    adjustedTemp = chatbot_get_temp(query).content
     newTemp = float(adjustedTemp)
 
-    llm = OpenAI(temperature=newTemp, openai_api_key=OPENAI_API_KEY)
+    llm = OpenAI(temperature=adjustedTemp, 
+                 openai_api_key=OPENAI_API_KEY)
 
     # Set up the RetrievalQA chain with the retriever
     qa_chain = RetrievalQA.from_chain_type(llm=llm,
                                   chain_type="stuff",
-                                  retriever=retriever,
+                                  retriever=doc_retriever,
                                   return_source_documents=True)
 
-
-
-
-    # query = "can you List all the street names beginning with the letter a"
-    # query1 = "whats 5 x 5?"
-
-
+    response = qa_chain.run(query)
     # parse_response(response)
     print ("\n*******************************************************\n")
-    #print (parse_response(qa_chain(query)))
+    print (parse_response(response))
     print ("\n*******************************************************\n")
     # print (chatbot_response_gpt(userFindTemp))
-    return jsonify({'reply': chatbot_response_gpt(userFindTemp).content})
-# print (response["source_documents"])
-
-print("Hello World!")
-
-
-
-
-
-
-
-
-
-
-# # Import the dependencies
-
-# from langchain.agents.agent_toolkits import (
-#     create_vectorstore_agent,
-#     VectorStoreToolkit,
-#     VectorStoreInfo,
-# )
-# # Set up the vectorstore info
-
-# vectorstore_info = VectorStoreInfo(
-#     name="Generative AI Reports",
-#     description="Reports on the State and Trends in Generative AI",
-#     vectorstore= docsearch,
-# )
-# # Setup the VectorStoreToolkit and VectorStore Agent
-
-# toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
-# agent_executor = create_vectorstore_agent(llm=llm, toolkit=toolkit, verbose=False)
-# # Add the string to ask for source
-
-# query = query + " List the sources."
-# print (query)
-# # Run the agent
-
-# response = agent_executor.run(query)
-# response
-
-
-
+    return jsonify({'reply': response["result"],
+                    'sourcedocs': response["source_documents"]})
 
 
 if __name__ == '__main__':
+    
+    # define file path for pdfs
+    myFilePath = './docs'
+    chatbot_vector_store(myFilePath)
     app.run(debug=True)
