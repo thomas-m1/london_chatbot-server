781d520cf4fd13b5f37c9c0f1fc597c7a782d10a
Dhruwang Akbari <dhruwang.akbari@gmail.com>
2024-02-29 17:13:02 -0500

    update pinecone.py

--- config/pinecone.py
+++ config/pinecone.py
@@ -1,2 +1,7 @@
-PINECONE_INDEX_NAME = 'your_index_name'
+import os
+
+OPENAI_API_KEY = "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"
+PINECONE_API_KEY = "2d3e984c-af4c-4b1f-9be2-7e90731a3ce4"
+PINECONE_ENV = "gcp-starter"
+PINECONE_INDEX_NAME = 'testingindex'
 PINECONE_NAME_SPACE = 'your_namespace'
