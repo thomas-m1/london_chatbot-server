df89ea1b3f71cf73247272c72f4b92f4a0dde4c1
Thomas Michalski <thomas_michalski@hotmail.com>
2024-03-12 18:09:19 -0400

    update

--- app.py
+++ app.py
@@ -16,6 +16,8 @@ from langchain_pinecone import PineconeVectorStore
 from langchain.chains.question_answering import load_qa_chain
 from pinecone import Pinecone as PineconeClient
 from pinecone import ServerlessSpec
+from langchain.prompts import PromptTemplate
+
 
 app = Flask(__name__)
 
@@ -95,7 +97,7 @@ def chatbot_get_temp(findTemp):
     completion = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "you are a virtual assistant"},
+        {"role": "system", "content": "You are a virtual assistant. You will be given a prompt from the user. Your objective is to analyze the prompt to determine what kind of tone you should respond to the user with. you are to give a rating on a scale of 0.0-1.0... 0.0 being a more severe situation that needs a prompt response, and a 1.0 being a less serious situation and can answer the phrase in a lighthearted manner. I only want the rating and nothing else."},
         {"role": "user", "content": qa_prompt},
     ]
     )
@@ -111,21 +113,34 @@ def chatbot_get_temp(findTemp):
 @app.route("/chatbot", methods=['POST'])
 def chatbot():
 
-    # doc_retriever
+    doc_retriever
     data = request.get_json()
     query = data['message']
     print("user message: ", query)
     adjustedTemp = chatbot_get_temp(query).content
-    adjustedTempFloat = float(adjustedTemp)
+    print("User query temperature: ", adjustedTemp)
 
-    llm = ChatOpenAI(temperature=adjustedTempFloat,
+    prompt_template = """Use the following pieces of context to answer the question at the end.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+    {context}
+
+    Question: {question}
+    Answer with context:
+    """
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+    llm = ChatOpenAI(temperature=adjustedTemp,
                  openai_api_key=OPENAI_API_KEY)
 
+    chain_kwargs = {"prompt": PROMPT}
     # Set up the RetrievalQA chain with the retriever
     qa_chain = RetrievalQA.from_chain_type(llm=llm,
                                   chain_type="stuff",
                                   retriever=doc_retriever,
-                                  return_source_documents=True)
+                                  return_source_documents=True,
+                                  chain_type_kwargs=chain_kwargs)
 
     result = qa_chain({"query": query})
     source_docs = []
@@ -137,9 +152,9 @@ def chatbot():
             'content': content,
             'doc' : f"Page {page_number}, Source: {source}"
         })
-    print(result["source_documents"])
-    print ("\n*******************************************************\n")
-    print ("\n*******************************************************\n")
+    # print(result["source_documents"])
+    print ("\n**\n")
+    print ("\n**\n")
     return jsonify({'reply': result["result"],
                     'source_docs': source_docs})
 
